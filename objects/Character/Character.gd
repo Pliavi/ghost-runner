c3be8ab7cb148ff17d73b6da60134567fbfb747b
pliavi <pliavi.rpgm@gmail.com>
2020-06-26 09:59:13 -0300

    make it jump higher and fix gravity

--- objects/Character/Character.gd
+++ objects/Character/Character.gd
@@ -2,12 +2,15 @@ extends KinematicBody2D
 
 const GRAVITY = 9.8 * 100
 var velocity = Vector2.ZERO
-const JUMP_HEIGHT = 150 * 100
+const JUMP_HEIGHT = 160 * 100
 
 func _physics_process(delta):
 	if is_on_floor() and Input.is_action_pressed("jump"):
 		self.jump()
-	velocity.y += GRAVITY
+		
+	if not is_on_floor():
+		velocity.y += GRAVITY
+		
 	self.move_and_slide(velocity * delta, Vector2.UP)
 
 func jump():
